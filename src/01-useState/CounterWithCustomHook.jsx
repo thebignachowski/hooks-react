c583935abf718d4cd22658ac962bbb3b0ffb962d
NachoRodriguez <nacho.rodriguezbe@gmail.com>
2022-11-21 19:01:31 +0100

    Comienzo Hooks-React

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,20 @@
+import { useCounter } from '../hooks/useCounter'
+
+export const CounterWithCustomHook = () => {
+
+    const { counter, increment, reset, decrement } = useCounter()
+    return (
+        <>
+            <h1>Counter with Hook: {counter}</h1>
+            <button className="btn btn-primary"
+                onClick={() => increment (2)}
+            >+1</button>
+            <button className="btn btn-primary"
+                onClick={reset}
+            >Reset</button>
+            <button className="btn btn-primary"
+                onClick={()=> decrement(2)}
+            >-1</button>
+        </>
+    )
+}
